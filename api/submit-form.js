0b57c744e09b5d1792619a01c5a79e1a49c0dcfa
Tirael009 <98650446+Tirael009@users.noreply.github.com>
2025-06-13 18:55:30 +0200

    Update submit-form.js

--- api/submit-form.js
+++ api/submit-form.js
@@ -19,6 +19,7 @@ export default async function handler(req, res) {
         body: JSON.stringify({
             name: name, 
             email: email, 
+            phone: phone,
             message: message
         }),
       });

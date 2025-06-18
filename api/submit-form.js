a71f70ae0e830b20d5066a5d23e9bc58a129b5f2
Tirael009 <98650446+Tirael009@users.noreply.github.com>
2025-06-18 02:05:11 +0200

    Update submit-form.js

--- api/submit-form.js
+++ api/submit-form.js
@@ -1,6 +1,10 @@
 export default async function handler(req, res) {
   // Устанавливаем заголовки для CORS
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Или замените '*' на ваш домен, если хотите ограничить доступ
+  const allowedOrigins = ['https://ivanmelnyk.dev', 'https://www.ivanmelnyk.dev'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 

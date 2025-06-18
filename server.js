baecbd6fc786c790fc4c1991e70a32c611f9bddb
Tirael009 <98650446+Tirael009@users.noreply.github.com>
2025-06-18 02:20:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
 
+require('dotenv').config();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+const submitForm = require('./api/submit-form');
+app.use('/api/submit-form', submitForm);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
